f12d2c55e15c126951c363994d8cc641b88c97c1
SI-tanbir <si017490@gmail.com>
2024-11-27 14:05:42 +0600

    fixing sign up buttion and sign up page

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,30 +1,41 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
+
 
 const Signup = () => {
+
+    let [show,setShow]=useState(false);
+    console.log(show);
   return (
-   
-      <form className="card-body w-[400px]">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn bg-yellow-300">SignUP</button>
-        </div>
-      </form>
-  
+    
+    <form className="card-body w-[400px]">
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Email</span>
+      </label>
+      <input type="email" placeholder="email" className="input input-bordered" required />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Password</span>
+      </label>
+      <div className='relative'>
+      <input type={ show?'text':'password'} placeholder="password" className="input input-bordered" required />
+      <button className='absolute top-3 right-2' onClick={()=>setShow(prev => !prev)} >{show?<IoMdEyeOff />:<IoMdEye />
+      }</button>
+      </div>
+
+
+      <label className="label">
+        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+      </label>
+    </div>
+    <div className="form-control mt-6">
+      <button className="btn bg-yellow-400  ">SignUP</button>
+    </div>
+  </form>
   )
 }
 
