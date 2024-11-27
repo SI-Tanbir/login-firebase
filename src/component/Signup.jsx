cccdeabfcd69923363b921dc73e7295ee4f44d93
SI-tanbir <si017490@gmail.com>
2024-11-27 17:50:07 +0600

    adding email forget password varification

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
 import app from "./firebase.config";
-import { getAuth, createUserWithEmailAndPassword,sendEmailVerification } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword,sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 
 const Signup = () => {
   const handleSignup = (e) => {
     e.preventDefault();
 
-    const email = e.target.email.value;
+    var email = e.target.email.value;
     const password = e.target.password.value;
 
     console.log(email, password);
@@ -37,6 +37,39 @@ const Signup = () => {
 
   let [show, setShow] = useState(false);
   // console.log(show);
+
+
+
+  const restEmail =(e)=>{
+    e.preventDefault();
+    const auth = getAuth(app);
+    const email = document.querySelector('input[name="email"]').value; 
+
+    if (!email) {
+        console.log("Please enter your email address to reset the password.");
+        return;
+      }
+
+
+      sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent
+        console.log("Password reset email sent!");
+      })
+
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(`Error: ${errorMessage} (Code: ${errorCode})`);
+      });
+
+
+
+  }
+
+
+
+
   return (
     <form onSubmit={handleSignup} className="card-b
     ody w-[400px]">
@@ -74,7 +107,7 @@ const Signup = () => {
         </div>
 
         <label className="label">
-          <a href="#" className="label-text-alt link link-hover">
+          <a onClick={restEmail} className="label-text-alt link link-hover">
             Forgot password?
           </a>
         </label>
