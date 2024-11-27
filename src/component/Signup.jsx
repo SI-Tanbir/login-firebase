9a3765a7085eaff5fbffe6d82287fdd375784d8b
SI-tanbir <si017490@gmail.com>
2024-11-27 16:49:55 +0600

    varification feature made in user regesters

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,48 +1,99 @@
-
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { IoMdEye } from "react-icons/io";
 import { IoMdEyeOff } from "react-icons/io";
-
+import app from "./firebase.config";
+import { getAuth, createUserWithEmailAndPassword,sendEmailVerification } from "firebase/auth";
 
 const Signup = () => {
+  const handleSignup = (e) => {
+    e.preventDefault();
 
-    let [show,setShow]=useState(false);
-    console.log(show);
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+
+    const auth = getAuth(app);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user)
+        sendEmailVerification(auth.currentUser)
+  .then(()=>{
+    // Email verification sent!
+    // ...
+    console.log("email varification sent")
+  });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        // ..
+      });
+  };
+
+  let [show, setShow] = useState(false);
+  // console.log(show);
   return (
-    
-    <form className="card-body w-[400px]">
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text">Email</span>
-      </label>
-      <input type="email" placeholder="email" className="input input-bordered" required />
-    </div>
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text">Password</span>
-      </label>
-      <div className='relative'>
-      <input type={ show?'text':'password'} placeholder="password" className="input input-bordered" required />
-      <button className='absolute top-3 right-2' onClick={()=>setShow(prev => !prev)} >{show?<IoMdEyeOff />:<IoMdEye />
-      }</button>
+    <form onSubmit={handleSignup} className="card-b
+    ody w-[400px]">
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Email</span>
+        </label>
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          className="input input-bordered"
+          required
+        />
       </div>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Password</span>
+        </label>
+        <div className="relative">
+          <input
+            type={show ? "text" : "password"}
+            placeholder="password"
+            className="input input-bordered"
+            name="password"
+            required
+          />
+          <button
+            type="button"
+            className="absolute top-3 right-2"
+            onClick={() => setShow((prev) => !prev)}
+          >
+            {show ? <IoMdEyeOff /> : <IoMdEye />}
+          </button>
+        </div>
 
-
-      <label className="label">
-        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-      </label>
-      <div className='flex'>
-      <input className='mr-3' type="checkbox" name="checked" id="" required/>
-      <p>Accept term and conditions</p>
-
-
+        <label className="label">
+          <a href="#" className="label-text-alt link link-hover">
+            Forgot password?
+          </a>
+        </label>
+        <div className="flex">
+          <input
+            className="mr-3"
+            type="checkbox"
+            name="checked"
+            id=""
+            required
+          />
+          <p>Accept term and conditions</p>
+        </div>
       </div>
-    </div>
-    <div className="form-control mt-6">
-      <button className="btn bg-yellow-400  ">SignUP</button>
-    </div>
-  </form>
-  )
-}
+      <div className="form-control mt-6">
+        <input className="btn bg-yellow-400  " type="submit" value="Signup" />
+      </div>
+    </form>
+  );
+};
 
 export default Signup;
---
+++ src/component/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM8t4lISfGzRLaP64W3hfINffVHaNkZ0E",
+  authDomain: "login-firebase-59296.firebaseapp.com",
+  projectId: "login-firebase-59296",
+  storageBucket: "login-firebase-59296.firebasestorage.app",
+  messagingSenderId: "1045299898968",
+  appId: "1:1045299898968:web:caf924d1941352ed92b82d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
